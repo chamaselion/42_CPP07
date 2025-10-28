2625ecf0b770b00ac4f67c6bbacd1fff3f04a04d
Balint Szikora <chamaselion@gmail.com>
2025-10-28 12:51:33 +0100

    Created class template and basic test for ints for now

---
+++ ex02/include/Array.hpp
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 14:16:57 by bszikora          #+#    #+#             */
+/*   Updated: 2025/10/28 12:50:00 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <exception>
+# include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+	T				*elements;
+	unsigned int	size;
+
+public:
+	Array();
+	Array(unsigned int n);
+	Array(const Array& other);
+	Array& operator=(const Array& other);
+	~Array();
+	
+	T& operator[](unsigned int index);
+	const T& operator[](unsigned int index) const;
+	
+	unsigned int size() const;
+	
+	class OutOfBoundsException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+};
+
+template <typename T>
+Array<T>::Array() : elements(NULL), size(0)
+{
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : elements(NULL), size(n)
+{
+	if (n > 0)
+	{
+		elements = new T[n];
+		for (unsigned int i = 0; i < n; i++)
+			elements[i] = T();
+	}
+}
+
+template <typename T>
+Array<T>::Array(const Array& other) : elements(NULL), size(other.size)
+{
+	if (size > 0)
+	{
+		elements = new T[size];
+		for (unsigned int i = 0; i < size; i++)
+			elements[i] = other.elements[i];
+	}
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array& other)
+{
+	if (this != &other)
+	{
+		delete[] elements;
+		size = other.size;
+		elements = NULL;
+		if (size > 0)
+		{
+			elements = new T[size];
+			for (unsigned int i = 0; i < size; i++)
+				elements[i] = other.elements[i];
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] elements;
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int index)
+{
+	if (index >= size)
+		throw OutOfBoundsException();
+	return elements[index];
+}
+
+template <typename T>
+const T& Array<T>::operator[](unsigned int index) const
+{
+	if (index >= size)
+		throw OutOfBoundsException();
+	return elements[index];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return size;
+}
+
+template <typename T>
+const char* Array<T>::OutOfBoundsException::what() const throw()
+{
+	return "Array index out of bounds";
+}
+
+#endif
---
+++ ex02/sources/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 14:16:46 by bszikora          #+#    #+#             */
+/*   Updated: 2025/10/28 12:47:25 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+#include <iostream>
+
+int main()
+{
+	// test default constructor
+	Array<int> empty;
+	std::cout << "Empty array size: " << empty.size() << std::endl;
+	
+	// test parameterized constructor
+	Array<int> arr(5);
+	std::cout << "Array size: " << arr.size() << std::endl;
+	
+	// Fill array with values
+	for (unsigned int i = 0; i < arr.size(); i++)
+		arr[i] = i * 2;
+	
+	// Display array contents
+	std::cout << "Array contents: ";
+	for (unsigned int i = 0; i < arr.size(); i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+	
+	// test copy constructor
+	Array<int> copy(arr);
+	std::cout << "Copy size: " << copy.size() << std::endl;
+	
+	// test assignment operator
+	Array<int> assigned;
+	assigned = arr;
+	std::cout << "Assigned size: " << assigned.size() << std::endl;
+	
+	// test bounds checking
+	try {
+		arr[10] = 42;
+	}
+	catch (const std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+	
+	return 0;
+}
